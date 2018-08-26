00e7d069bdf69cd8def4f0fb8bfaad4db8474ee6
Raulvgz <raulvgz@hotmail.com>
2018-08-26 09:42:01 +0200

    Subida inicial

---
+++ main.js
@@ -0,0 +1,24 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+// Cargar el modulo HTTP
+var http = require('http');
+ 
+// Configurar una respuesta HTTP para todas las peticiones
+function onRequest(request, response) {
+  console.log("Peticion Recibida.");
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write("Hola Mundo");
+  response.end();
+}
+ 
+var server = http.createServer(onRequest);
+ 
+// Escuchar al puerto 8080
+server.listen(8080);
+ 
+// Poner un mensaje en la consola
+console.log("Servidor funcionando en http://localhost:8080/");
